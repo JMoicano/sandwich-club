2ae64bc75e758b6b22de0e30589d6a5602225e1f
Moicano <jmariossilva@gmail.com>
2019-04-16 22:24:03 -0300

    Layout
    Layou adjusted and method to populate UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -17,12 +18,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mAlsoKnownTv;
+    private TextView mOriginTv;
+    private TextView mIngredientsTv;
+    private TextView mDescriptionTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mAlsoKnownTv = findViewById(R.id.also_known_tv);
+        mOriginTv = findViewById(R.id.origin_tv);
+        mIngredientsTv = findViewById(R.id.ingredients_tv);
+        mDescriptionTv = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -50,7 +60,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +73,14 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        for (String s : sandwich.getAlsoKnownAs()){
+            mAlsoKnownTv.append(s + "\n");
+        }
+        mOriginTv.setText(sandwich.getPlaceOfOrigin());
+        for (String s : sandwich.getIngredients()){
+            mIngredientsTv.append(s + "\n");
+        }
+        mDescriptionTv.setText(sandwich.getDescription());
     }
 }
